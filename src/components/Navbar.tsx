d87f8a7496fbb3925e5303b311009b804496606c
JekBurri <onorbtanko@gmail.com>
2024-02-24 00:31:48 -0800

    fixed navbar px

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     const {view, setView} = useContext(AppContext);
 
     return(
-        <div className="flex justify-between bottom-0 red-espresso text-white font-bold p-2 px-20">
+        <div className="flex justify-between bottom-0 red-espresso text-white font-bold p-2 px-12">
             <div onClick={()=>setView({...view, page:"home"})} className="flex gap-2 flex-col justify-center">
                 <img src="/navbar/home.png" className="h-10" alt="" />
                 <span className="text-center">Home</span>
