88ff2d0ec7d6e42ac775f47618e9c7fb1d122e83
JekBurri <onorbtanko@gmail.com>
2024-02-24 17:03:11 -0800

    Add background color and shadow to QRCode component

--- src/components/QRCode.tsx
+++ src/components/QRCode.tsx
@@ -1,6 +1,6 @@
 export default function QRCode() {
   return (
-    <div className="p-4">
+    <div className="p-4 bg-white rounded-lg shadow-sm">
       <img className="h-auto" src="/QRCode.png" alt="qrcode to scan for points" />
     </div>
   );
