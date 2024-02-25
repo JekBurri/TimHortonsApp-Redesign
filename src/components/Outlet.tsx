55d49707c21885a97a957c569e67c5b3201d0b83
JekBurri <onorbtanko@gmail.com>
2024-02-24 16:36:48 -0800

    Remove unused components and update imports

--- src/components/Outlet.tsx
+++ src/components/Outlet.tsx
@@ -2,11 +2,9 @@ import { useContext } from "react";
 import { AppContext } from "../App";
 import Home from "./Home";
 import Deals from "./Deals";
-import Scan from "./Scan";
 import Order from "./Order";
-import Checkout from "./Checkout";
-import Cart from "./Cart";
 import Header from "./Header";
+import Cart from "./Cart";
 
 export default function Outlet() {
   const { view } = useContext(AppContext);
@@ -16,18 +14,15 @@ export default function Outlet() {
     <div className="flex-1 overflow-y-auto">
       {view.page === "home" ? (
         <Home />
-      ) : view.page === "scan" ? (
-        <Scan />
       ) : view.page === "deals" ? (
         <Deals />
       ) : view.page === "order" ? (
         <Order />
-      ) : view.page === "checkout" ? (
-        <Checkout />
       ) : view.page === "cart" ? (
         <Cart />
       ) : (
         <div>404</div>
+
       )}
     </div>
     </>);
