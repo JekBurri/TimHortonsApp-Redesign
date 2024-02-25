5c97274fd1da8c88919dbd6dc9a5aead2d66cfb5
JekBurri <onorbtanko@gmail.com>
2024-02-24 16:36:26 -0800

    Fixed icon styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,16 +7,16 @@ export default function Navbar() {
 
     return(
         <div className="flex justify-between bottom-0 red-espresso text-white font-bold p-2 px-12">
-            <div onClick={()=>setView({...view, page:"home"})} className="flex gap-2 flex-col justify-center">
-                <img src="/navbar/home.png" className="h-10" alt="" />
+            <div onClick={()=>setView({...view, page:"home"})} className="flex flex-col justify-center">
+                <img src="/navbar/home.png" className="flex self-center justify-center align-middle  h-10" alt="" />
                 <span className="text-center">Home</span>
             </div>
-            <div onClick={()=>setView({...view, page:"order"})} className="flex gap-2 flex-col justify-center">
-                <img src="/navbar/paper-cup.png" className="h-10" alt="" />
+            <div onClick={()=>setView({...view, page:"order"})} className="flex flex-col justify-center">
+                <img src="/navbar/paper-cup.png" className="flex self-center justify-center align-middle h-10" alt="" />
                 <span className="text-center">Menu</span>
             </div>
-            <div onClick={()=>setView({...view, page:"deals"})} className="flex gap-2 flex-col justify-center">
-                <img src="/navbar/discount.png" className="h-10" alt="" />
+            <div onClick={()=>setView({...view, page:"deals"})} className="flex flex-col justify-center">
+                <img src="/navbar/discount.png" className="flex self-center justify-center align-middle h-10" alt="" />
                 <span className="text-center">Deals</span>
             </div>
         </div>
