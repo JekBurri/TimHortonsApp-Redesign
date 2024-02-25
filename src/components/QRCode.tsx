75cf1a47b4c3df4bc799b8f3d9a48972878643ec
JekBurri <onorbtanko@gmail.com>
2024-02-24 17:10:04 -0800

    Refactor QRCode component structure

--- src/components/QRCode.tsx
+++ src/components/QRCode.tsx
@@ -1,7 +1,10 @@
 export default function QRCode() {
   return (
-    <div className="p-4 bg-white rounded-lg shadow-sm">
-      <img className="h-auto" src="/QRCode.png" alt="qrcode to scan for points" />
+    <div className="p-4">
+      <div className="p-4 bg-white rounded-lg shadow-sm">
+        <img className="h-auto" src="/QRCode.png" alt="qrcode to scan for points" />
+
+      </div>
     </div>
   );
 }
